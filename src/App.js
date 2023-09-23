a14aece4b40b6f0e90ee1969b2939bfa2bc4c464
Ben Ross <mrbenross@gmail.com>
2023-09-23 16:06:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>You can remove thisssss code and create your own header</p>
     </>
   );
 };
