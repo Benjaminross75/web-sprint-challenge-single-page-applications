2821ebf1a37973d0ca74cc7f22f5ced869f91920
Ben Ross <mrbenross@gmail.com>
2023-09-25 16:43:57 -0500

    size-drop-down-added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import {useState, useEffect} from "react"
 import {Routes, Route, } from 'react-router-dom'
 import Home from './Home'
 import Pizza from './Pizza'
+const initialValues = {
+  choiceOfSize: '',
+  choiceOfSauce:'',
+  pepperoni: false,
+  sausage: false,
+  canadianBacon: false,
+  onions:false,
+  glutenFree: false,
+  specialInstructions:''
 
+}
 export default function App(){
-
+const [formValues, setFormValues] = useState(initialValues)
 
   return(
     <div className="container">
       <h1>Bloomtech Eats</h1>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/pizza' element={<Pizza/>}/>
+        <Route path='/pizza' element={<Pizza values={formValues}/>}/>
       </Routes>
     </div>
   )
--- src/Pizza.js
+++ src/Pizza.js
@@ -1,18 +1,33 @@
-
-export default function Pizza(){
+ const style = {margin: '2px', padding:'2px', backgroundColor:'lightblue'}
+ const style2 = {color:'red'}
+export default function Pizza(values){
+    console.log(values)
     const onSubmit = () =>{
         console.log("subbmitted")
     }
+
+    const onChange = evt =>{
+        console.log(evt)
+    }
     return(
-        <div>
-            <h1>Pizza Form</h1>
+        <div >
+            <h3>Build Your Own Pizza</h3>
             <form onSubmit={onSubmit} id="order-pizza">
-             <input id="name-input"
-               name="name"
-               type='text'
-               placeholder="Your Name"
-               
-             />
+            <div style={style}>
+                <h3 style={style}>Choice of size</h3>
+                <h4 style={style2}>required</h4>
+                </div>
+                <select id="size-dropdown"
+                    onChange={onChange}
+                    value={values}
+                    name='choiceOfSize'
+                    >
+                        <option value=''> --- Select an option</option>
+                        <option value='small'>Small</option>
+                        <option value='medium'>Medium</option>
+                        <option value='large'>Large</option>
+                </select>
+
             </form>
         </div>
     )
